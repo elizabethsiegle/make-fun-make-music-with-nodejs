5675d3a50b9bc2196f3d917859f8c8bbe80a741e
Lizzie Siegle <esiegle@brynmawr.edu>
2019-01-24 13:48:26 -0800

    edit for forwardjs

--- talk.js
+++ talk.js
@@ -1,10 +1,39 @@
-const scribble = require('scribbletune')
+const scribble = require('scribbletune');
 
-console.log(scribble.chords())
+var note = scribble.clip({
+	notes: ['c4']
+});
+scribble.midi(note, 'note.mid');
+
+var notes = scribble.clip({
+	notes: ['c1', 'd1', 'e1', 'f1', 'g1', 'a1', 'b1'],
+	pattern: 'x-x_'.repeat(4)
+});
+scribble.midi(notes, 'notes.mid');
+
+var scale = scribble.clip({
+	notes: scribble.scale('a4 Major'),
+	pattern: 'x-'.repeat(7)
+});
+scribble.midi(scale, 'scale.mid');
+
+console.log(scribble.scales());
+var scale2 = scribble.clip({
+	notes: scribble.scale('a4 Major').filter((x, y) => y%2 == 1),
+	pattern: 'x-'.repeat(7)
+});
+scribble.midi(scale2, 'scale2.mid');
+
+console.log(scribble.chords());
 var chord = scribble.clip({
-	notes: ['c# +add#9'],
-	pattern: 'x-x_'.repeat(2),
+	notes: ['C# m11'],
+	pattern: 'x_'.repeat(4)
+})
+
+var arp = scribble.clip({
+	notes: scribble.arp('CM EM GM'),
+	pattern: 'x_'.repeat(4),
+	subdiv: '16n' //optional
 });
 
-
-scribble.midi(chord, 'chord.mid')
+scribble.midi(arp, 'arp.mid');
